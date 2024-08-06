bdc012c12ef9c6053d83bf600ed1977d9f1cbbf5
Veids <veidsme@gmail.com>
2024-08-06 14:46:51 +0300

    bump deps

--- go.mod
+++ go.mod
@@ -5,17 +5,17 @@ go 1.21.1
 require (
 	github.com/hashicorp/yamux v0.1.1
 	github.com/pkg/sftp v1.13.6
-	github.com/things-go/go-socks5 v0.0.4
-	golang.org/x/net v0.20.0
-	google.golang.org/grpc v1.60.1
-	google.golang.org/protobuf v1.32.0
+	github.com/things-go/go-socks5 v0.0.5
+	golang.org/x/net v0.27.0
+	google.golang.org/grpc v1.65.0
+	google.golang.org/protobuf v1.34.2
 )
 
 require (
-	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/kr/fs v0.1.0 // indirect
-	golang.org/x/crypto v0.18.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240108191215-35c7eff3a6b1 // indirect
+	golang.org/x/crypto v0.25.0 // indirect
+	golang.org/x/sys v0.23.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240805194559-2c9e96a0b5d4 // indirect
 )
--- tools/fwdctrl.py
+++ tools/fwdctrl.py
@@ -5,7 +5,9 @@ import argparse
 from tools.clientpb import client_pb2_grpc
 from tools.clientpb import client_pb2
 from tools.commonpb import common_pb2
+
 empty = common_pb2.Empty()
+
 
 def list_endpoints(args):
     with grpc.insecure_channel(args.control) as channel:
@@ -14,8 +16,9 @@ def list_endpoints(args):
         endpoints = stub.List(empty).endpoints
         print("\n".join(endpoints))
 
+
 def add_socks(args):
-    host,port = args.listen.split(':')
+    host, port = args.listen.split(":")
     addr = common_pb2.Addr()
     addr.ip, addr.port = host, int(port)
 
@@ -27,8 +30,9 @@ def add_socks(args):
         except grpc.RpcError as rpc_error:
             print(rpc_error.details())
 
+
 def rm_socks(args):
-    host,port = args.listen.split(':')
+    host, port = args.listen.split(":")
     addr = common_pb2.Addr()
     addr.ip, addr.port = host, int(port)
 
@@ -40,9 +44,10 @@ def rm_socks(args):
         except grpc.RpcError as rpc_error:
             print(rpc_error.details())
 
+
 def add_reverse(args):
-    host,port = args.listen.split(':')
-    remote_host,remote_port = args.remote.split(':')
+    host, port = args.listen.split(":")
+    remote_host, remote_port = args.remote.split(":")
     addrPack = common_pb2.AddrPack()
     addrPack.local.ip, addrPack.local.port = host, int(port)
     addrPack.remote.ip, addrPack.remote.port = remote_host, int(remote_port)
@@ -55,9 +60,10 @@ def add_reverse(args):
         except grpc.RpcError as rpc_error:
             print(rpc_error.details())
 
+
 def rm_reverse(args):
-    host,port = args.listen.split(':')
-    remote_host,remote_port = args.remote.split(':')
+    host, port = args.listen.split(":")
+    remote_host, remote_port = args.remote.split(":")
     addrPack = common_pb2.AddrPack()
     addrPack.local.ip, addrPack.local.port = host, int(port)
     addrPack.remote.ip, addrPack.remote.port = remote_host, int(remote_port)
@@ -69,6 +75,7 @@ def rm_reverse(args):
             stub.ReverseStop(addrPack.remote)
         except grpc.RpcError as rpc_error:
             print(rpc_error.details())
+
 
 def list_files(args):
     p = client_pb2.Path()
@@ -83,6 +90,7 @@ def list_files(args):
         except grpc.RpcError as rpc_error:
             print(rpc_error.details())
 
+
 def glob_files(control, path: str):
     p = client_pb2.Path()
     p.path = path
@@ -96,6 +104,7 @@ def glob_files(control, path: str):
             print(rpc_error.details())
 
     return []
+
 
 def download_file(control, path: str, out_dir: str):
     req = client_pb2.FileTransferRequest()
@@ -149,8 +158,8 @@ def upload(args):
 
 
 def add_forward(args):
-    host,port = args.listen.split(':')
-    remote_host,remote_port = args.remote.split(':')
+    host, port = args.listen.split(":")
+    remote_host, remote_port = args.remote.split(":")
     addrPack = common_pb2.AddrPack()
     addrPack.local.ip, addrPack.local.port = host, int(port)
     addrPack.remote.ip, addrPack.remote.port = remote_host, int(remote_port)
@@ -163,8 +172,9 @@ def add_forward(args):
         except grpc.RpcError as rpc_error:
             print(rpc_error.details())
 
+
 def rm_forward(args):
-    host,port = args.listen.split(':')
+    host, port = args.listen.split(":")
 
     addr = common_pb2.Addr()
     addr.ip, addr.port = host, int(port)
@@ -177,20 +187,30 @@ def rm_forward(args):
         except grpc.RpcError as rpc_error:
             print(rpc_error.details())
 
+
 def main():
-    parser = argparse.ArgumentParser(
-        prog = "fwdctrl.py"
+    parser = argparse.ArgumentParser(prog="fwdctrl.py")
+
+    parser.add_argument(
+        "-c",
+        "--control",
+        metavar="127.0.0.1:8337",
+        default="127.0.0.1:8337",
+        help="Control server address",
     )
-
-    parser.add_argument("-c", '--control', metavar = "127.0.0.1:8337", default = "127.0.0.1:8337", help = "Control server address")
-
 
     main_sub = parser.add_subparsers(title="Commands")
 
     socks_parser = main_sub.add_parser("socks")
     socks_sub = socks_parser.add_subparsers()
-    socks_listen_addr = argparse.ArgumentParser(add_help = False)
-    socks_listen_addr.add_argument("-l", "--listen", metavar="127.0.0.1:1080", default = "127.0.0.1:1080", help = "Listen address")
+    socks_listen_addr = argparse.ArgumentParser(add_help=False)
+    socks_listen_addr.add_argument(
+        "-l",
+        "--listen",
+        metavar="127.0.0.1:1080",
+        default="127.0.0.1:1080",
+        help="Listen address",
+    )
     add_parser = socks_sub.add_parser("add", parents=[socks_listen_addr])
     add_parser.set_defaults(func=add_socks)
     rm_parser = socks_sub.add_parser("rm", parents=[socks_listen_addr])
@@ -198,48 +218,80 @@ def main():
 
     reverse_parser = main_sub.add_parser("reverse")
     reverse_sub = reverse_parser.add_subparsers()
-    remote_listen_addr = argparse.ArgumentParser(add_help = False)
-    remote_listen_addr.add_argument("-l", "--listen", metavar="127.0.0.1:8445", help = "Local connect address", required = True)
-    remote_addr = argparse.ArgumentParser(add_help = False)
-    remote_addr.add_argument("-r", "--remote", metavar="127.0.0.1:8445", help = "Remote listen address", required = True)
-    r_add_parser = reverse_sub.add_parser("add", parents=[remote_listen_addr, remote_addr])
+    remote_listen_addr = argparse.ArgumentParser(add_help=False)
+    remote_listen_addr.add_argument(
+        "-l",
+        "--listen",
+        metavar="127.0.0.1:8445",
+        help="Local connect address",
+        required=True,
+    )
+    remote_addr = argparse.ArgumentParser(add_help=False)
+    remote_addr.add_argument(
+        "-r",
+        "--remote",
+        metavar="127.0.0.1:8445",
+        help="Remote listen address",
+        required=True,
+    )
+    r_add_parser = reverse_sub.add_parser(
+        "add", parents=[remote_listen_addr, remote_addr]
+    )
     r_add_parser.set_defaults(func=add_reverse)
-    r_rm_parser = reverse_sub.add_parser("rm", parents=[remote_listen_addr, remote_addr])
+    r_rm_parser = reverse_sub.add_parser(
+        "rm", parents=[remote_listen_addr, remote_addr]
+    )
     r_rm_parser.set_defaults(func=rm_reverse)
 
     forward_parser = main_sub.add_parser("forward")
     forward_sub = forward_parser.add_subparsers()
-    forward_remote_listen_addr = argparse.ArgumentParser(add_help = False)
-    forward_remote_listen_addr.add_argument("-l", "--listen", metavar="127.0.0.1:8445", help = "Local connect address", required = True)
-    forward_remote_addr = argparse.ArgumentParser(add_help = False)
-    forward_remote_addr.add_argument("-r", "--remote", metavar="127.0.0.1:8445", help = "Remote listen address", required = True)
-    r_add_parser = forward_sub.add_parser("add", parents=[forward_remote_listen_addr, forward_remote_addr])
+    forward_remote_listen_addr = argparse.ArgumentParser(add_help=False)
+    forward_remote_listen_addr.add_argument(
+        "-l",
+        "--listen",
+        metavar="127.0.0.1:8445",
+        help="Local connect address",
+        required=True,
+    )
+    forward_remote_addr = argparse.ArgumentParser(add_help=False)
+    forward_remote_addr.add_argument(
+        "-r",
+        "--remote",
+        metavar="127.0.0.1:8445",
+        help="Remote listen address",
+        required=True,
+    )
+    r_add_parser = forward_sub.add_parser(
+        "add", parents=[forward_remote_listen_addr, forward_remote_addr]
+    )
     r_add_parser.set_defaults(func=add_forward)
-    r_rm_parser = forward_sub.add_parser("rm", parents=[forward_remote_listen_addr, forward_remote_addr])
+    r_rm_parser = forward_sub.add_parser(
+        "rm", parents=[forward_remote_listen_addr, forward_remote_addr]
+    )
     r_rm_parser.set_defaults(func=rm_forward)
 
     list_parser = main_sub.add_parser("list")
     list_parser.set_defaults(func=list_endpoints)
 
     list_files_parser = main_sub.add_parser("ls")
-    list_files_parser.add_argument("path", metavar=".", help = "Remote directory path")
+    list_files_parser.add_argument("path", metavar=".", help="Remote directory path")
     list_files_parser.set_defaults(func=list_files)
 
     glob_parser = main_sub.add_parser("glob")
-    glob_parser.add_argument("path", metavar=".", help = "Remote directory path")
+    glob_parser.add_argument("path", metavar=".", help="Remote directory path")
     glob_parser.set_defaults(func=glob)
 
     get_parser = main_sub.add_parser("get")
-    get_parser.add_argument("path", metavar=".", help = "Remote file path")
+    get_parser.add_argument("path", metavar=".", help="Remote file path")
     get_parser.set_defaults(func=download)
 
     get_glob_parser = main_sub.add_parser("get_glob")
-    get_glob_parser.add_argument("path", metavar=".", help = "Remote file path")
+    get_glob_parser.add_argument("path", metavar=".", help="Remote file path")
     get_glob_parser.set_defaults(func=download_glob)
 
     get_parser = main_sub.add_parser("put")
-    get_parser.add_argument("input", help = "Local file path")
-    get_parser.add_argument("output", help = "Remote file path")
+    get_parser.add_argument("input", help="Local file path")
+    get_parser.add_argument("output", help="Remote file path")
     get_parser.set_defaults(func=upload)
 
     args = parser.parse_args()
